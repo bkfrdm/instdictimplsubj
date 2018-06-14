5147f5c647c01ed59e7f95c1ae2fd5a4bb5dad48
bkfrdm <coolpythonista@gmail.com>
2018-06-14 16:25:30 +0300

    push test to ghub, edit .gitignore

--- inst_to_dict_meta.py
+++ inst_to_dict_meta.py
@@ -1,7 +1,5 @@
-class Meta(type):
+class MetaDict(type):
     def __call__(cls, *args):
-        print('Meta.__call__:', cls, args, sep='\n\t')
-        
         data = cls.main(*args)
         
         new_cls = type(
@@ -10,19 +8,24 @@ class Meta(type):
             {k: v for k, v in cls.__dict__.items() if not k == '__init__'})
         
         instance = new_cls(data)
+        instance = cls.rebind_init_values(instance, *args)
+        
+        return instance
+
+    def rebind_init_values(cls, instance, *args):
         for n, v in zip(cls.__init__.__code__.co_names, args):
             # __dict__ descriptor is broken -> TypeError
             setattr(instance, n, v)
-            
+
         return instance
 
       
-class DictTools:
+class DictToolsMix:
     def pops(self, *keys):
         return {key: self.pop(key) for key in keys}
 
-    
-class InstToDict(dict, DictTools, metaclass=Meta):
+
+class InstToDict(dict, DictToolsMix, metaclass=MetaDict):
     def __init__(self, filename):
         self.filename = filename
 
@@ -35,46 +38,3 @@ class InstToDict(dict, DictTools, metaclass=Meta):
     def main(cls, *args):
         filename, = args
         return cls.data_parser(filename)
-
-
-
-
-if __name__ == '__main__':
-    filename = 'spam'
-    I = InstToDict(filename)
-
-    print('-'*50, '\ndefined class instance returns dict object')
-    
-    assert I == {k: ord(k) for k in 'spam'}, f'Failed I: {I}'
-    print('I:', I, end='\n\n')
-
-    print('-'*50, '\ndefined class instance __init__ value persists')
-    
-    assert I.filename == filename, f'Failed I.filename: {I.filname}'
-    print('I.filename:', I.filename, end='\n\n')
-
-
-    print('-'*50, '\ndefined class mixin methods inhereted')
-    
-    popped = I.pops('a', 'm')
-    test = {k: ord(k) for k in 'am'}
-    assert popped == test, f'Failed I.pops: {popped} != {test}'
-    print('I.pops:', popped, end='\n\n')
-
-    test = {k: ord(k) for k in 'sp'}
-    assert I == test, f'Failed I: {I} != {test}'
-    print('I:', I, end='\n\n')
-
-
-    print('-'*50, '\ndict class builtins inhereted')
-
-    I.update(f=56, q=55)
-    test = {'s': 115, 'p': 112, 'f': 56, 'q': 55}
-    assert I == test, f'Failed I: {I} != {test}'
-    print('I.update:', I, end='\n\n')
-    
-    poped = I.pop('q')
-    test = {'s': 115, 'p': 112, 'z': 56, 'q': 55}.pop('q')
-    assert poped == test, f'Failed I.pop: {poped} != {test}'
-    print('I.pop:', poped)
-    print('I:', I, end='\n\n')
---
+++ tests.py
@@ -0,0 +1,57 @@
+import unittest
+from random import choice
+
+from inst_to_dict_meta import InstToDict, DictToolsMix
+
+
+
+
+TEST_OUTPUT_FILENAME = None
+FILENAME = 'excomunication'
+
+
+class Test_InstToDict(unittest.TestCase):
+    def get_obj_to_test(self, value):
+        return InstToDict(value), {c: ord(c) for c in value}
+    
+    def test_instance_returns_dict_type(self):
+        instance, _ = self.get_obj_to_test(FILENAME)
+        self.assertTrue(isinstance(instance, dict))
+
+    def test_instance_returns_expected_dict(self):
+        instance, test_dict = self.get_obj_to_test(FILENAME)
+        self.assertEqual(instance, test_dict)
+
+    def test_instance_retains_standart_dict_methods(self):
+        instance, test_dict = self.get_obj_to_test(FILENAME)
+
+        for o in (instance, test_dict):
+            o.update({1:22, 's': 'spam'})
+        self.assertEqual(instance, test_dict)
+
+        self.assertFalse(set(dir(test_dict)) - set(dir(instance)))
+
+    def test_InstToDict_retains_mixin_methods(self):
+        keys_to_pop = set(choice(FILENAME) for t in range(len(FILENAME)//2))
+        
+        instance, test_dict = self.get_obj_to_test(FILENAME)
+
+        instance.pops(*keys_to_pop)
+        {test_dict.pop(k) for k in keys_to_pop}
+        self.assertEqual(instance, test_dict)
+
+        self.assertFalse(set(DictToolsMix.__dict__) - set(dir(instance)))
+
+    def test_InstToDict_retains_init_values(self):
+        instance, test_dict = self.get_obj_to_test(FILENAME)
+        
+        self.assertEqual(instance.filename, FILENAME)
+
+        with self.assertRaises(AttributeError):
+            test_dict.filename
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
